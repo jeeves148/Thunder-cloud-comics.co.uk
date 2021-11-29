21e976959b8e836318036e7f71c17d70c0d9c380
Jeeves <83597947+jeeves148@users.noreply.github.com>
2021-11-29 22:14:25 +0000

    Dark mode and picture added
    Cause pebble liked it
    and pzazz

--- public/whibityJqueryInteractivity.js
+++ public/whibityJqueryInteractivity.js
@@ -5,7 +5,31 @@ $(document).ready(function(){
 
     $("h1").click(function(){
         $("h1").append(hiWorld);
-        
-
     });
-});
+
+    //Dark switch script
+
+    let dark = false;
+
+        $("#dark-switch").click(function(){
+            if (dark == false)
+            {
+                $("body").get(0).style.setProperty("--blank-space", "black");
+                $("body").get(0).style.setProperty("--text-color", "white");
+                $("body").get(0).style.setProperty("--background-color", "black");
+                $("body").get(0).style.setProperty("--topnav-color", "white");
+                dark = true;
+            }
+            else if(dark == true)
+            {
+                $("body").get(0).style.setProperty("--blank-space", "white");
+                $("body").get(0).style.setProperty("--text-color", "black");
+                $("body").get(0).style.setProperty("--background-color", "#fffd82");
+                $("body").get(0).style.setProperty("--topnav-color", "#333");  
+                dark = false;              
+            }
+    });
+
+
+    
+});//end of document ready so really don't write any code outside of this
